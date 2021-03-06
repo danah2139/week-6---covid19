8bed4c0529589c42fcc2ecfc986878dea5236404
dana cohen <danah2139@gmail.com>
2021-03-06 19:51:35 +0200

    finish get data from api and create data chart

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let proxy = 'https://api.allorigins.win/raw?url=';
-let covidUrl = 'https://corona-api.com/countries';
-//let covidPerCounteryUrl = `https://corona-api.com/countries/${countryCode}`;
+let worldCovidUrl = 'https://corona-api.com/countries';
 let countriesUrl = `${proxy}https://restcountries.herokuapp.com/api/v1`;
+let covidPerCounteryUrl = `https://corona-api.com/countries/${countryCode}`;
 
 async function fetchData(url) {
 	try {
@@ -22,48 +22,89 @@ async function createWorldObj() {
 	for (let countery of data) {
 		world[countery.region] = continent;
 		continent[countery.cca2] = countery.name.common;
-		//console.log(countery.cca2);
-		//console.log(countery.region);
 	}
-	console.log(world);
+	return world;
 }
 
-// async function getCountryData() {}
+function createCountryObj(countryData) {
+	if (countryData) {
+		// console.log(countryData);
+		const countryObj = {
+			name: countryData.data.name,
+			code: countryData.data.code,
+			confirmed: countryData.data.latest_data.confirmed,
+			newConfirmed: countryData.data.today.confirmed,
+			critical: countryData.data.latest_data.critical,
+			deaths: countryData.data.latest_data.deaths,
+			newDeaths: countryData.data.today.deaths,
+			recovered: countryData.data.latest_data.recovered,
+		};
+		return countryObj;
+	}
+}
 
-// async function createChartData(continent, infoType) {}
+async function createChartData(continent, infoType) {
+	// go over country codes in given continent. for each code fetch relevant info and country name. put each in the relevant array.
+	spinnerContainerElement.classList.remove('hidden');
+	let dataLabelsArray = [];
+	let dataValuesArray = [];
+	let dataCodeArray = [];
+	if (continent === 'world') {
+		const continentFullCoronaData = await fetchData(worldCovidUrl);
+		// save name and value of infoType given
+		for (let country of continentFullCoronaData.data) {
+			dataCodeArray.push(country.code);
+			dataLabelsArray.push(country.name);
+			dataValuesArray.push(country.latest_data[infoType]);
+		}
+	} else {
+		// save name and value of infoType given
+		const continent = world[continent];
+		for (let country of continent) {
+			const countryCode = country.cca2;
+			let covidPerCounteryUrl = `https://corona-api.com/countries/${countryCode}`;
+			countryCoronaData = await fetchData(covidPerCounteryUrl);
+			if (countryCoronaData) {
+				dataCodeArray.push(code);
+				dataLabelsArray.push(countryCoronaData.data.name);
+				dataValuesArray.push(countryCoronaData.data.latest_data[infoType]);
+			}
+		}
+	}
+}
 
-// function createNewChart(currentData, infoType, continent) {
-// 	const covidChart = document.createElement('canvas');
-// 	covidChart.setAttribute('id', '#covidChart');
-// 	chartContainer.appendChild(covidChart);
-// 	newChartInstance = new Chart(covidChart, {
-// 		type: 'line',
-// 		data: {
-// 			labels: currentData.dataLabels,
-// 			datasets: [
-// 				{
-// 					label: `${infoType} in ${continent}`,
-// 					backgroundColor: '#1d2d506e',
-// 					borderColor: '#133b5c',
-// 					borderWidth: '1',
-// 					data: currentData.dataValues,
-// 				},
-// 			],
-// 		},
-// 		options: {
-// 			maintainAspectRatio: false,
-// 			scales: {
-// 				yAxes: [
-// 					{
-// 						ticks: {
-// 							beginAtZero: true,
-// 						},
-// 					},
-// 				],
-// 			},
-// 		},
-// 	});
-// 	return covidChart;
-// }
+function createNewChart(currentData, infoType, continent) {
+	const covidChart = document.createElement('canvas');
+	covidChart.setAttribute('id', '#covidChart');
+	chartContainer.appendChild(covidChart);
+	newChartInstance = new Chart(covidChart, {
+		type: 'line',
+		data: {
+			labels: currentData.dataLabels,
+			datasets: [
+				{
+					label: `${infoType} in ${continent}`,
+					backgroundColor: '#1d2d506e',
+					borderColor: '#133b5c',
+					borderWidth: '1',
+					data: currentData.dataValues,
+				},
+			],
+		},
+		options: {
+			maintainAspectRatio: false,
+			scales: {
+				yAxes: [
+					{
+						ticks: {
+							beginAtZero: true,
+						},
+					},
+				],
+			},
+		},
+	});
+	return covidChart;
+}
 
 createWorldObj();
